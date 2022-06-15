76082c684ed3d46ba468831acb9672a5b0167529
CoolSpy3 <coolspythree@gmail.com>
2022-06-14 18:25:49 -0700

    bug fixes

--- kernel/VGA/driver.asm
+++ kernel/VGA/driver.asm
@@ -7,176 +7,192 @@ dump_state: ; probably unnecessary,
 dump_regs: ; probably unnecessary,
            ; but if I have a more basic print function
            ; it might be useful.
-    push cl
-    add empty_VGAregister_data_buffer, 8
+    ; push cx
+    ; add empty_VGAregister_data_buffer, 1
 
     ; semantics(just a ton of reg dump loop calls)
-    ; to lazy to finish it
+    ; too lazy to finish it
 
-    pop cl
+    ; pop cx
     ret
 
 reg_dump_loop:
-    push ch
-    mov ch, 0
+    ; push cx
+    ; mov ch, 0
 
-    reg_loop0:
-        inc ch
-        cmp ch, 8
-        jl reg_loop0_maybe
+    ; reg_loop0:
+    ;     inc ch
+    ;     cmp ch, 8
+    ;     jl reg_loop0_maybe
 
-        mov ch, 0
+    ;     mov ch, 0
 
-        reg_loop0_maybe:
-        add empty_VGAregister_data_buffer, 8
-        dec cl
-        cmp cl, 0
-        jne reg_loop0
-        
+    ;     reg_loop0_maybe:
+    ;     add empty_VGAregister_data_buffer, 8
+    ;     dec cl
+    ;     cmp cl, 0
+    ;     jne reg_loop0
 
 
-    pop ch
+    ; pop cx
     ret
 
 read_regs: ; probably unnecessary,
            ; but if I have a more basic print function
            ; it might be useful.
-    push ax
-    push si
-    push di
-    push ch
+    ; push ax
+    ; push si
+    ; push di
+    ; push ch
 
-    in ax, VGA_misc_read
-    mov [empty_VGAregister_data_buffer], ax
+    ; in ax, VGA_misc_read
+    ; mov [empty_VGAregister_data_buffer], ax
 
-    add empty_VGAregister_data_buffer, 8
+    ; add empty_VGAregister_data_buffer, 1
 
-    mov si, VGA_seq_index ; VGA_index
-    mov di, VGA_seq_data ; VGA_data
-    mov ch, 5 ; loop end condition
-    call reg_read_loop ; call
+    ; mov si, VGA_seq_index ; VGA_index
+    ; mov di, VGA_seq_data ; VGA_data
+    ; mov ch, 5 ; loop end condition
+    ; call reg_read_loop ; call
 
-    mov si, VGA_crtr_index
-    mov di, VGA_crtr_data
-    mov ch, 25
-    call reg_read_loop
+    ; mov si, VGA_crtc_index
+    ; mov di, VGA_crtc_data
+    ; mov ch, 25
+    ; call reg_read_loop
 
-    mov si, VGA_GC_index
-    mov di, VGA_GC_data
-    mov ch, 9
-    call reg_read_loop
+    ; mov si, VGA_GC_index
+    ; mov di, VGA_GC_data
+    ; mov ch, 9
+    ; call reg_read_loop
 
 
-    mov si, VGA_AC_index
-    mov di, VGA_AC_read
-    mov ch, 21
-    call reg_read_loop
+    ; mov si, VGA_AC_index
+    ; mov di, VGA_AC_read
+    ; mov ch, 21
+    ; call reg_read_loop
 
-    in ax, VGA_instat_read
-    out VGA_AC_index, 0x20
+    ; in ax, VGA_instat_read
+    ; out VGA_AC_index, 0x20
 
-    pop ax
-    pop si
-    pop di
-    pop ch
+    ; pop ax
+    ; pop si
+    ; pop di
+    ; pop ch
     ret
 
-    
 reg_read_loop: ;si: VGA_index, di: VGA_data, ch: loop end condition
-    push cl ; counter
-    mov cl, 0 ; set to 0
+    ; push cl ; counter
+    ; mov cl, 0 ; set to 0
 
-    out si, cl
-    in ax, di
-    mov [empty_VGAregister_data_buffer], ax
-    inc cl
-    add empty_VGAregister_data_buffer, 8
+    ; out si, cl
+    ; in ax, di
+    ; mov [empty_VGAregister_data_buffer], ax
+    ; inc cl
+    ; add empty_VGAregister_data_buffer, 1
 
-    cmp cl, ch
-    jne reg_read_loop
+    ; cmp cl, ch
+    ; jne reg_read_loop
 
-    pop cl
+    ; pop cl
     ret
 
 write_regs:
-    push ax
-    push si
-    push di
-    push ch
+    pushad
 
-    out VGA_misc_write, [VGAregister_data]
-    add VGAregister_data, 8
+    mov dx, VGA_misc_write
+    mov ebx, VGAregister_data
+    mov al, [ebx]
+    out dx, al
+
+    inc ebx
 
     mov si, VGA_seq_index
     mov di, VGA_seq_data
-    mov ch, 5
-
-    ; unlock crtc registers
-    out VGA_crtr_index, 0x03
-    in ax, VGA_crtr_data
-    or ax, 0x80
-    out VGA_crtr_data, ax
-
-
-    out VGA_crtr_index, 0x11
-    in ax, VGA_crtr_data
-
-    mov si, 0x80
-    not si
-    and ax, si
-
-    out VGA_crtr_data, ax
-    ;
-
-    sub VGAregister_data, 37
-    mov si, 0x80
-    not si
-    and [VGAregister_data], si
-
-    sub VGAregister_data, 8
-    or [VGAregister_data], 0x80
-    add VGAregister_data, 45
-
-
-    mov si, VGA_crtr_index
-    mov di, VGA_crtr_data
-    mov ch, 25
+    mov cl, 0
+    mov ch, VGA_num_seq_regs
     call write_reg_loop
 
+    ; unlock crtc registers
+    mov dx, VGA_crtc_index
+    mov al, 0x03
+    out dx, al
+    mov dx, VGA_crtc_data
+    in al, dx
+    or al, 0x80
+    mov dx, VGA_crtc_data
+    out dx, al
 
+    mov dx, VGA_crtc_index
+    mov al, 0x11
+    out dx, al
 
-    pop ax
-    pop si
-    pop di
-    pop ch
+    mov dx, VGA_crtc_data
+    in al, dx
+    and al, ~0x80
+    out dx, al
+
+    mov al, [VGAregister_data+0x03]
+    or al, 0x80
+    mov [VGAregister_data+0x03], al
+
+    mov al, [VGAregister_data+0x11]
+    and al, ~0x80
+    mov [VGAregister_data+0x11], al
+
+    mov si, VGA_crtc_index
+    mov di, VGA_crtc_data
+    mov cl, 0
+    mov ch, VGA_num_crtc_regs
+    call write_reg_loop
+
+    mov si, VGA_GC_index
+    mov di, VGA_GC_data
+    mov cl, 0
+    mov ch, VGA_num_GC_regs
+    call write_reg_loop
+
+    mov si, VGA_AC_index
+    mov di, VGA_AC_write
+    mov cl, 0
+    mov ch, VGA_num_AC_regs
+    call write_reg_loop
+
+    popad
     ret
 
-write_reg_loop: ;si: VGA_index, di: VGA_data, ch: loop end condition
-    push cl
-    mov cl, 0
+write_reg_loop: ;si: VGA_index, di: VGA_data, ch: loop end condition, cl: 0, ebx: start of VGAregister_data (will be incremented)
+    mov dx, si
+    mov al, cl
+    out dx, al
+    mov dx, di
+    mov al, [ebx]
+    out dx, al
 
-    out si, cl
-    out di, [VGAregister_data]
-
+    inc ebx
     inc cl
     cmp cl, ch
-    jne write_reg_loop
-
-    pop cl
+    jl write_reg_loop
     ret
 
-VGA_seq_index db 0x3c4
-VGA_seq_data db 0x3c5
-VGA_crtr_index db 0x3d4
-VGA_crtr_data db 0x3d5
-VGA_GC_index db 0x3ce
-VGA_GC_data db 0x3cf
-VGA_AC_index db 0x3c0
 
-VGA_instat_read db 0x3da
-VGA_AC_read db 0x3c1
-VGA_misc_read db 0x3cc
-VGA_misc_write db 0x3c2
+VGA_seq_index equ 0x3c4
+VGA_seq_data equ 0x3c5
+VGA_crtc_index equ 0x3d4
+VGA_crtc_data equ 0x3d5
+VGA_GC_index equ 0x3ce
+VGA_GC_data equ 0x3cf
+VGA_AC_index equ 0x3c0
+
+VGA_instat_read equ 0x3da
+VGA_AC_read equ 0x3c1
+VGA_AC_write equ 0x3c0
+VGA_misc_read equ 0x3cc
+VGA_misc_write equ 0x3c2
+
+VGA_num_seq_regs equ 5
+VGA_num_crtc_regs equ 25
+VGA_num_GC_regs	equ 9
+VGA_num_AC_regs	equ 21
 
 empty_VGAregister_data_buffer:
     ;miscellaneous register(1)
@@ -335,4 +351,3 @@ VGAregister_data:
     db 0x00
     db 0x00
     ;
-
