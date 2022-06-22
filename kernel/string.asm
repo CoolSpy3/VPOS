8534549de091e06176e13acca284f31d7636987d
CoolSpy3 <coolspythree@gmail.com>
2022-06-22 15:04:54 -0700

    add substr

--- kernel/string.asm
+++ kernel/string.asm
@@ -9,7 +9,25 @@ str_len: ; Put string addr in edi, length returned in eax
     pop ecx
     ret
 
-substr: ; copies esi to a new string retuned in edi from [eax,ebx)
+substr: ; copies esi to a new string retuned in edi (ecx bytes starting from idx eax)
+    push esi
+    push eax
     push ecx
-    
+
+    add esi, eax
+    mov eax, ecx
+    add eax, 1
+    call malloc
+    mov edi, eax
+
+    push edi
+    cld
+    rep movsb
+    pop edi
+
+    add edi, ecx
+    mov [edi], 0
+
     pop ecx
+    pop eax
+    pop esi
